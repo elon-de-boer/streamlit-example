2ebbdd72bcdabc2209c89a1e9ee7213653b176fb
elon-de-boer <elon.de.boer@gmail.com>
2023-05-16 11:33:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,12 +17,12 @@ st.set_page_config(layout="wide")
 
 row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.columns((.1, 2.3, .1, 1.3, .1))
 with row0_1:
-    st.title('BuLiAn - Bundesliga Analyzer')
+    st.title('Caldomus')
 with row0_2:
     st.text("")
-    st.subheader('Streamlit App by [Tim Denzler](https://www.linkedin.com/in/tim-denzler/)')
+    st.subheader('Interactieve web interface door [Innoforte](https://www.innoforte.nl)')
 row3_spacer1, row3_1, row3_spacer2 = st.columns((.1, 3.2, .1))
 with row3_1:
-    st.markdown("Hello there! Have you ever spent your weekend watching the German Bundesliga and had your friends complain about how 'players definitely used to run more' ? However, you did not want to start an argument because you did not have any stats at hand? Well, this interactive application containing Bundesliga data from season 2013/2014 to season 2019/2020 allows you to discover just that! If you're on a mobile device, I would recommend switching over to landscape for viewing ease.")
-    st.markdown("You can find the source code in the [BuLiAn GitHub Repository](https://github.com/tdenzl/BuLiAn)")
-    st.markdown("If you are interested in how this app was developed check out my [Medium article](https://tim-denzler.medium.com/is-bayern-m%C3%BCnchen-the-laziest-team-in-the-german-bundesliga-770cfbd989c7)")
+    st.markdown("In deze web applicatie is het mogelijk om te bekijken wat de impact is van isolatie en renovatie voor uw gasverbruik, elektraverbruik2, co2 uitstoot én uw portemonnee")
+    st.markdown("Elon")
+    st.markdown("^^^^^^^")
